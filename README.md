6d6327134f581388b9e9cf894eb1095ebbe66050
Mike Innes <mike.j.innes@gmail.com>
2015-07-09 11:15:21 +0100

    init

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
-# PrivateRyan
+# PrivateRyan.jl
 
-[![Build Status](https://travis-ci.org/one-more-minute/PrivateRyan.jl.svg?branch=master)](https://travis-ci.org/one-more-minute/PrivateRyan.jl)
+```julia
+Pkg.checkout("Lazy")
+Pkg.clone("http://github.com/one-more-minute/PrivateRyan.jl")
+```
+
+ *PrivateRyan.jl* implements public and private field access for Julia.
+Consider the following example:
+
+```julia
+module Test
+using PrivateRyan
+@___!!checked!!___ type Foo
+  @free willy
+  @__!!private!!1__ ryan
+end
+
+const foo = Foo(0, 0)
+
+foo.willy = 5
+@___!!checked!!___ foo.ryan = 5 # Accesses to private vars are always
+                                # qualified.
+end
+```
+
+As is clear from evaluating `Test.foo` in the repl, the `Test` module is
+able to access and modify all fields of `Foo`. However, encapsulation
+is enforced; so while free `willy` is able to roam through modules as he
+wishes, you can save private `ryan` from damage on the front of foreign
+modules. For example:
+
+```julia
+julia> using PrivateRyan
+
+julia> Test.foo
+Test.Foo(5,5)
+
+julia> Test.foo.willy
+5
+
+julia> Test.foo.ryan
+ERROR: type Foo has no field ryan
+
+julia> @___!!checked!!___ Test.foo.ryan
+ERROR: Test.foo.ryan can only be accessed from Test
+ in error at ./error.jl:21
+```
--- src/PrivateRyan.jl
+++ src/PrivateRyan.jl
@@ -1,5 +1,57 @@
 module PrivateRyan
 
-# package code goes here
+using Lazy
+
+export @___!!checked!!___
+
+const private = symbol("@__!!private!!1__")
+const public = symbol("@free")
+
+secretname(n) = symbol("#__$(n)__")
+
+mod(T) = error("T is not checked.")
+
+macro ___!!checked!!___(ex)
+  if isexpr(ex, :type)
+    name = namify(ex.args[2])
+    for (i, field) in enumerate(ex.args[3].args)
+      isexpr(field, :line, LineNumberNode) && continue
+      (isexpr(field, :macrocall) && field.args[1] in [public, private]) ||
+        error("Field must be labelled as public or private")
+      ispub = field.args[1] == public
+      fieldname = namify(field.args[2])
+      !ispub && (field.args[2] = replace(field.args[2], fieldname, secretname(fieldname)))
+      ex.args[3].args[i] = field.args[2]
+    end
+    quote
+      $(esc(ex))
+      PrivateRyan.mod(::$(esc(name))) = $(current_module())
+      $(esc(name))
+    end
+
+  elseif isexpr(ex, :.)
+    T = esc(ex.args[1])
+    f = secretname(ex.args[2].args[1])
+    mod = current_module()
+    quote
+      $mod == mod($T) ||
+        error("$($(Expr(:quote, ex))) can only be accessed from $(mod($T))")
+      $T.$f
+    end
+
+  elseif isexpr(ex, :(=)) && isexpr(ex.args[1], :.)
+    T = esc(ex.args[1].args[1])
+    f = secretname(ex.args[1].args[2].args[1])
+    mod = current_module()
+    quote
+      $mod == mod($T) ||
+        error("$($(Expr(:quote, ex))) can only be accessed from $(mod($T))")
+      $T.$f = $(esc(ex.args[2]))
+    end
+
+  else
+    error("Unsupported syntax")
+  end
+end
 
 end # module
